3691c39c75cd68afb5f3849c6d2178cbdd9adec1
ari <kadekarik45@gmail.com>
2019-12-07 04:31:32 +0800

    db

---
+++ app/src/main/java/com/example/apparelproject/database/DatabaseHelper.java
@@ -0,0 +1,95 @@
+package com.example.apparelproject.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.example.apparelproject.utils.Config;
+import com.orhanobut.logger.AndroidLogAdapter;
+import com.orhanobut.logger.Logger;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static DatabaseHelper databaseHelper;
+
+    // All Static variables
+    private static final int DATABASE_VERSION =1 ;
+
+    // Database Name
+    private static final String DATABASE_NAME = Config.DATABASE_NAME;
+
+    // Constructor
+    private DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        Logger.addLogAdapter(new AndroidLogAdapter());
+    }
+
+    public static synchronized DatabaseHelper getInstance(Context context){
+        if(databaseHelper==null){
+            databaseHelper = new DatabaseHelper(context);
+        }
+        return databaseHelper;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        // Create tables SQL execution
+        String CREATE_PRODUCT_TABLE = "CREATE TABLE " + Config.TABLE_PRODUCT + "("
+                + Config.COLUMN_PRODUCT_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + Config.COLUMN_PRODUCT_TOKO + " TEXT , "
+                + Config.COLUMN_PRODUCT_CATEGORYPRODUCT + " TEXT , "
+                + Config.COLUMN_PRODUCT_NAME + " TEXT , "
+                + Config.COLUMN_PRODUCT_MERK + " TEXT , "
+                + Config.COLUMN_PRODUCT_HARGA + " INTEGER , "
+                + Config.COLUMN_PRODUCT_QTY + " INTEGER , "
+                + Config.COLUMN_PRODUCT_DESC + " TEXT "
+                + ");";
+        // Create tables SQL execution
+        String CREATE_CATEGORYPRODUCT_TABLE = "CREATE TABLE " + Config.TABLE_CATEGORY_PRODUCT + "("
+                + Config.COLUMN_CATEGORY_PRODUCT_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + Config.COLUMN_CATEGORY_PRODUCT_NAME + " TEXT "
+                + ");";
+        // Create tables SQL execution
+        String CREATE_TOKO = "CREATE TABLE " + Config.TABLE_TOKO + "("
+                + Config.COLUMN_TOKO_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + Config.COLUMN_TOKO_NAME + " TEXT , "
+                + Config.COLUMN_TOKO_ALAMAT + " TEXT , "
+                + Config.COLUMN_TOKO_NOTELP + " TEXT "
+                + ");";
+        // Create tables SQL execution
+        String CREATE_PEGAWAI_TABLE = "CREATE TABLE " + Config.TABLE_PEGAWAI + "("
+                + Config.COLUMN_PEGAWAI_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + Config.COLUMN_PEGAWAI_TOKO + " TEXT , "
+                + Config.COLUMN_PEGAWAI_DEVISI + " TEXT , "
+                + Config.COLUMN_PEGAWAI_NAME + " TEXT , "
+                + Config.COLUMN_PEGAWAI_ALAMAT + " INTEGER , "
+                + Config.COLUMN_PEGAWAI_NOTELP + " INTEGER "
+                + ");";
+        // Create tables SQL execution
+        String CREATE_DEVISI_TABLE = "CREATE TABLE " + Config.TABLE_DEVISI + "("
+                + Config.COLUMN_DEVISI_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + Config.COLUMN_DEVISI_NAME + " TEXT "
+                + ");";
+        db.execSQL(CREATE_PRODUCT_TABLE);
+        db.execSQL(CREATE_CATEGORYPRODUCT_TABLE);
+        db.execSQL(CREATE_TOKO);
+        db.execSQL(CREATE_PEGAWAI_TABLE);
+        db.execSQL(CREATE_DEVISI_TABLE);
+
+        Logger.d("DB created!");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // Drop older table if existed
+        db.execSQL("DROP TABLE IF EXISTS " + Config.TABLE_PRODUCT);
+        db.execSQL("DROP TABLE IF EXISTS " + Config.TABLE_TOKO);
+        db.execSQL("DROP TABLE IF EXISTS " + Config.TABLE_CATEGORY_PRODUCT);
+        db.execSQL("DROP TABLE IF EXISTS " + Config.TABLE_PEGAWAI);
+        db.execSQL("DROP TABLE IF EXISTS " + Config.TABLE_DEVISI);
+        // Create tables again
+        onCreate(db);
+    }
+
+}
---
+++ app/src/main/java/com/example/apparelproject/utils/Config.java
@@ -0,0 +1,61 @@
+package com.example.apparelproject.utils;
+
+public class Config {
+
+    public static final String DATABASE_NAME = "db_toko";
+
+    //column names of product table
+    public static final String TABLE_PRODUCT = "tb_product";
+    public static final String COLUMN_PRODUCT_ID = "_id";
+    public static final String COLUMN_PRODUCT_CATEGORYPRODUCT = "category_product";
+    public static final String COLUMN_PRODUCT_TOKO = "toko";
+    public static final String COLUMN_PRODUCT_NAME = "name";
+    public static final String COLUMN_PRODUCT_HARGA = "harga";
+    public static final String COLUMN_PRODUCT_MERK = "merk";
+    public static final String COLUMN_PRODUCT_QTY = "qty";
+    public static final String COLUMN_PRODUCT_DESC = "deskripsi";
+
+    //column names of devisi table
+    public static final String TABLE_DEVISI = "tb_devisi";
+    public static final String COLUMN_DEVISI_ID = "_id";
+    public static final String COLUMN_DEVISI_NAME = "name";
+
+    //column names of category product table
+    public static final String TABLE_CATEGORY_PRODUCT = "tb_category_product";
+    public static final String COLUMN_CATEGORY_PRODUCT_ID = "_id";
+    public static final String COLUMN_CATEGORY_PRODUCT_NAME = "name";
+
+    //column names of pegawai table
+    public static final String TABLE_PEGAWAI = "tb_pegawai";
+    public static final String COLUMN_PEGAWAI_ID = "_id";
+    public static final String COLUMN_PEGAWAI_TOKO = "toko";
+    public static final String COLUMN_PEGAWAI_DEVISI = "devisi";
+    public static final String COLUMN_PEGAWAI_NAME = "name";
+    public static final String COLUMN_PEGAWAI_ALAMAT = "alamat";
+    public static final String COLUMN_PEGAWAI_NOTELP = "no_telp";
+
+    //column names of toko table
+    public static final String TABLE_TOKO = "tb_toko";
+    public static final String COLUMN_TOKO_ID = "_id";
+    public static final String COLUMN_TOKO_NAME = "name";
+    public static final String COLUMN_TOKO_ALAMAT = "alamat";
+    public static final String COLUMN_TOKO_NOTELP = "no_telp";
+
+
+    //others for general purpose key-value pair data
+    public static final String TITLE = "title";
+    public static final String CREATE_PRODUCT = "create_product";
+    public static final String UPDATE_PRODUCT = "update_product";
+
+    public static final String CREATE_TOKO = "create_toko";
+    public static final String UPDATE_TOKO = "update_toko";
+
+    public static final String CREATE_PEGAWAI = "create_pegawai";
+    public static final String UPDATE_PEGAWAI = "update_pegawai";
+
+    public static final String CREATE_CATEGORY_PRODUCT = "create_category_product";
+    public static final String UPDATE_CATEGORY_PRODUCT = "update_category_product";
+
+    public static final String CREATE_DEVISI = "create_devisi";
+    public static final String UPDATE_DEVISI = "update_devisi";
+}
